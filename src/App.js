714ae761e825ec40120be9f9570b3f608391833b
Glen Wu <glenwu1998@gmail.com>
2024-06-05 15:41:22 -0400

    completed calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const [result, setResult] = useState(0);
+  const [inputVal, setInputVal] = useState(null);
+  const inputRef = useRef(inputVal);
+
+  function plus(e) {
+    e.preventDefault();
+    setResult((res) => res + inputVal);
+  }
+
+  function minus(e) {
+    e.preventDefault();
+    setResult((res) => res - inputVal);
+  }
+
+  function times(e) {
+    e.preventDefault();
+    setResult((res) => res * inputVal);
+  }
+
+  function divide(e) {
+    e.preventDefault();
+    setResult((res) => res / inputVal);
+  }
+
+  function resetInput(e) {
+    e.preventDefault();
+    inputRef.current.value = "";
+    inputRef.current.focus();
+    setInputVal("");
+  }
+
+  function resetResult(e) {
+    e.preventDefault();
+    setResult(0);
+  }
+
+  function inputChangeHandler(e) {
+    if (e.target.value.length > 0) {
+      setInputVal(Number(e.target.value));
+    } else {
+      setInputVal(null);
+    }
+  }
+
+  let disabled = typeof inputVal != "number";
+  let divisionDisable = inputVal === 0;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <h2>
+          <span>{result}</span>
+        </h2>
+        <input
+          ref={inputRef}
+          onChange={inputChangeHandler}
+          pattern="[0-9]"
+          type="number"
+          placeholder="Type a number"
+        />
+        <button disabled={disabled} onClick={plus}>
+          add
+        </button>
+        <button disabled={disabled} onClick={minus}>
+          subtract
+        </button>
+        <button disabled={disabled} onClick={times}>
+          multiply
+        </button>
+        <button disabled={disabled || divisionDisable} onClick={divide}>
+          divide
+        </button>
+        <button disabled={disabled} onClick={resetInput}>
+          reset input
+        </button>
+        <button disabled={disabled} onClick={resetResult}>
+          reset result
+        </button>
+      </form>
     </div>
   );
 }
